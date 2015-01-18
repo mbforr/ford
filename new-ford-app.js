8be40a2a7f6b5af22a9c8956c16da822a575dc5e
Matt Huntington <matt.huntington@gmail.com>
2015-01-18 13:22:07 -0500

    moving info div within map div

--- new-ford-app.js
+++ new-ford-app.js
@@ -66,8 +66,9 @@ $.getJSON('http://'+account_name+'.cartodb.com/api/v2/sql/?q='+sql_statement, fu
 		var num_maps = $('body > .map').length;
 		divID += num_maps+1;
 		infodivID += num_maps+1;
-		$('body').append('<div class="map" id="'+divID+'"></div><br/>');
-		$('body').append('<div class="info" id="'+infodivID+'"></div>');
+		$('<div class="map" id="'+divID+'"></div><br/>')
+			.appendTo('body')
+			.append('<div class="info" id="'+infodivID+'"></div>');
 
 		var self = this;
 		var InitialCenter = new L.LatLng(40, 0);
