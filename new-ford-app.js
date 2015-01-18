f705fa4497458dc80ac1c5649c681ded13db3e76
Matt Huntington <matt.huntington@gmail.com>
2015-01-18 13:03:27 -0500

    create as many maps as you want

--- new-ford-app.js
+++ new-ford-app.js
@@ -63,6 +63,10 @@ $.getJSON('http://'+account_name+'.cartodb.com/api/v2/sql/?q='+sql_statement, fu
 		$('#'+divID).remove();
 	}
 	this.createMap = function(divID, infodivID) {
+		var num_maps = $('body > div[id^="'+divID+'"]').length;
+		divID += num_maps+1;
+		infodivID += num_maps+1;
+		console.log(divID);
 		$('body').append('<div id="'+divID+'"></div><br/>');
 		$('body').append('<div id="'+infodivID+'"></div>');
 
