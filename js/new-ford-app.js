ec0af5bf5d8e6d1824e23e2629c1a017b50df9c6
Matt Huntington <matt.huntington@gmail.com>
2015-02-15 19:51:11 -0500

    making map colors configurable through mapstyles.css

--- js/new-ford-app.js
+++ js/new-ford-app.js
@@ -1,22 +1,21 @@
-function cartoMap(query, divID, colors) { 
+function cartoMap(query, divID) { 
+
 	//configurable values
 	var account_name = 'sgcm',
 		delimiter = ' && ',
 		southWest = L.latLng(80, 120),
 		northEast = L.latLng(-30, -120);
 
-
-	// initial cartoCSS for National and Subnational levels
-	var cssDictionary = ['#006D2C', '#2CA25F', '#66C2A4', '#B2E2E2', '#EDF8FB'];// color ramp,
-		borderColor = '#FFF',
-		polygonFillColor = '#EDF8FB';
  
  	//private vars
 	var cssInitial_Nat,
 		cssInitial_SubNat,
 		$container,
 		natsublayer,
-		subnatsublayer;
+		subnatsublayer,
+		cssDictionary,
+		borderColor,
+		polygonFillColor;
 
 	var splitParam = function(param, delimiter){
 		var ix = param.indexOf(delimiter);
@@ -24,7 +23,6 @@ function cartoMap(query, divID, colors) {
 		var param2 = param.substring(ix + delimiter.length, param.length);
 		return {natQuery:param1, subnatQuery:param2};
 	}
-
 
 	function createDonnutChart(value, national_value){
 		var chart = c3.generate({
@@ -50,12 +48,23 @@ function cartoMap(query, divID, colors) {
 		});
 	};
 
+	var readColor = function (styleName) {
+        var div = $("<div class='" + styleName + "' style='visibility:hidden' />");
+        $("body").append(div);
+        var color = $(div).css("background-color");
+        $(div).remove();
+		return color;
+	}
+
 	var createMap = function () {
-		if(colors !== null && typeof colors == "object") {
-			cssDictionary = colors.cssDictionary;
-			borderColor = colors.borderColor;
-			polygonFillColor = colors.polygonFillColor;
-		}
+		cssDictionary = [
+			readColor('mapColor1'), 
+			readColor('mapColor2'), 
+			readColor('mapColor3'), 
+			readColor('mapColor4'), 
+			readColor('mapColor5')];
+		borderColor = readColor('mapBorderColor');
+		polygonFillColor = readColor('mapPolygonFillColor');
 		cssInitial_Nat = "[zoom<=3]{#world_borders{  polygon-fill: "+polygonFillColor+";  polygon-opacity: 0.8;  line-color: "+borderColor+";  line-width: 1;  line-opacity: 1;}#world_borders [ total_amount <= 1781668262.38] {   polygon-fill: " + cssDictionary[0] + ";}#world_borders [ total_amount <= 31403245] {   polygon-fill:" + cssDictionary[1] + ";}#world_borders [ total_amount <= 3713003.72] {   polygon-fill: " + cssDictionary[2] + ";}#world_borders [ total_amount <= 1144718] {   polygon-fill:" + cssDictionary[3] + ";} #world_borders [ total_amount <= 400000] {   polygon-fill: " + cssDictionary[4] + ";}}";
 		cssInitial_SubNat = "[zoom>3]{#subnat{  polygon-fill: "+polygonFillColor+";  polygon-opacity: 0.8;  line-color: "+borderColor+";  line-width: 1;  line-opacity: 1;}#subnat [ total_amount <= 118349410.5] {   polygon-fill:" + cssDictionary[0] + ";}#subnat [ total_amount <= 11477333] {   polygon-fill: " + cssDictionary[1] +";}#subnat [ total_amount <= 4490000] {   polygon-fill: " + cssDictionary[2] + ";}#subnat [ total_amount <= 1485000] {   polygon-fill: " + cssDictionary[3] + ";}#subnat [ total_amount <= 500000] {   polygon-fill: " + cssDictionary[4] + ";}}";
  
